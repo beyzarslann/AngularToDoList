e71992ff1cf975e990e47b1255342fb23b667141
beyzarslann <beyzarslan865@gmail.com>
2024-01-05 20:30:36 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-list';
+
+
+  inputText: string = '';
+  hedefler: string[] = [];
+  silinenler: string[] = [];
+  isEditMode = false;
+
+
+
+  addValue() {
+    this.hedefler.push(this.inputText);
+    this.inputText = '';
+  }
+
+  editItem(index: number) {
+    this.inputText = this.hedefler[index];
+    if (!this.isEditMode) this.isEditMode = true;
+    else this.isEditMode = false;
+  }
+
+  deleteItem(index: number) {
+    const item = this.hedefler[index];
+    this.silinenler.push(item);
+    this.hedefler.splice(index, 1);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
